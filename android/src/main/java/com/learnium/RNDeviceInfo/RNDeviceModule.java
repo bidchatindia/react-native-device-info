d37c9020df89556b0c35ae3d8c4ae15f47a08add
Dhanraj Naik <dhanraj.naik@bidchat.com>
2019-07-05 15:28:32 +0530

    fix library issue

--- android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
+++ android/src/main/java/com/learnium/RNDeviceInfo/RNDeviceModule.java
@@ -409,10 +409,12 @@ public class RNDeviceModule extends ReactContextBaseJavaModule {
   public void isLocationEnabled(Promise p) {
       boolean locationEnabled = false;
 
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-        LocationManager mLocationManager = (LocationManager) reactContext.getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-        locationEnabled = mLocationManager.isLocationEnabled();
-      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+      //   LocationManager mLocationManager = (LocationManager) reactContext.getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+      //   locationEnabled = mLocationManager.isLocationEnabled();
+      // } else 
+      
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
         int locationMode = Settings.Secure.getInt(reactContext.getContentResolver(), Settings.Secure.LOCATION_MODE, Settings.Secure.LOCATION_MODE_OFF);
         locationEnabled = locationMode != Settings.Secure.LOCATION_MODE_OFF;
       } else {
